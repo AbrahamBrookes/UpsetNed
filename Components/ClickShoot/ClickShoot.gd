21c1237370fe87cdf02ca4bd252387b00ba7c6f7
Abraham Brookes <abrahambrookes@gmail.com>
2025-10-26 23:01:34 +1000

    pistolae

--- Components/ClickShoot/ClickShoot.gd
+++ Components/ClickShoot/ClickShoot.gd
@@ -20,6 +20,10 @@ var sliding: bool = false
 # the anim tree for looking
 @export var anim_tree: AnimationTree
 
+# a reference to the left and right weapons
+@export var l_weapon: Node3D
+@export var r_weapon: Node3D
+
 func _ready():
 	pass
 	
@@ -29,34 +33,40 @@ func _physics_process(_delta: float) -> void:
 	anim_tree.set("parameters/StandingAimLHandBlend/blend_amount", 0)
 	anim_tree.set("parameters/SlidingAimLHandBlend/blend_amount", 0)
 	anim_tree.set("parameters/SlidingAimRHandBlend/blend_amount", 0)
-	print("sliding ", sliding)
-	print("r_shooting ", r_shooting)
-	print("l_shooting ", l_shooting)
+	
 	if r_shooting:
 		if sliding:
 			anim_tree.set("parameters/SlidingAimRHandBlend/blend_amount", 1)
-			print("slide shoot R")
+			#print("slide shoot R")
 		else:
 			anim_tree.set("parameters/StandingAimRHandBlend/blend_amount", 1)
-			print("stand shoot R")
+			#print("stand shoot R")
 	if l_shooting:
 		if sliding:
 			anim_tree.set("parameters/SlidingAimLHandBlend/blend_amount", 1)
-			print("slide shoot L")
+			#print("slide shoot L")
 		else:
 			anim_tree.set("parameters/StandingAimLHandBlend/blend_amount", 1)
-			print("stand shoot L")
+			#print("stand shoot L")
 	
 
 # when the player clicks fire_r or fire_l, toggle the related flag
 func _input(event):
 	if event.is_action_pressed("fire_r"):
-		r_shooting = true
-		r_timer.start()
+		fire_r()
 	elif event.is_action_pressed("fire_l"):
-		l_shooting = true
-		l_timer.start()
+		fire_l()
+		
+func fire_r():
+	r_shooting = true
+	r_timer.start()
+	r_weapon.fire()
 
+func fire_l():
+	l_shooting = true
+	l_timer.start()
+	l_weapon.fire()
+	
 func _on_r_timer_timeout():
 	r_shooting = false
 
