bd558f3b73967080af201216ab6a25b58876ed1e
Abraham Brookes <abrahambrookes@gmail.com>
2026-01-02 23:40:17 +1000

    add main menu with play button

--- Scripts/Bootstrap/ClientBootstrapper.gd
+++ Scripts/Bootstrap/ClientBootstrapper.gd
@@ -5,6 +5,15 @@ extends Node
 ## loads up the game start UI and other scripts take it from there.
 class_name ClientBootstrapper
 
+## the world root is where we load maps into
+@export var world_root: Node
+
 # bootstrap services
 func boot() -> void:
 	print("bootstrapping client app")
+	
+	# load up the main menu
+	var main_menu = load("res://UI/MainMenu/MainMenu.tscn").instantiate()
+	main_menu.name = "Main Menu"
+	world_root.add_child(main_menu)
+	
