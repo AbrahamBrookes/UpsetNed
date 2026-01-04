e07973ed0e370ffc1682cd2ffc4df1e36eedd533
Abraham Brookes <abrahambrookes@gmail.com>
2026-01-04 16:11:56 +1000

    add an editor mesh for spawnpoints

--- Maps/SpawnPoint.gd
+++ Maps/SpawnPoint.gd
@@ -3,3 +3,10 @@ extends Node3D
 ## A SpawnPoint is a node that spawns players. This is just to have a type-safe
 ## node for spawning players
 class_name SpawnPoint
+
+## the mesh we can see in the editor so we know where the spawn points are
+@export var mesh: MeshInstance3D
+
+func _ready() -> void:
+	# hide the mesh in game
+	mesh.visible = false
